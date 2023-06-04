9a529d8144e6c6eae0560bf2d85b2ae57eb064b9
Lamberto Martinez <lambpato@gmail.com>
2023-06-04 23:03:36 +0000

    created form

---
+++ client/src/components/NewSong.jsx
@@ -0,0 +1,36 @@
+export default function NewSong () {
+
+  return (
+    <div className="container">
+      <div className="row min-vh-100 pb-5 justify-content-center align-items-center">
+        <div className="col col-md-8">
+          <h3 className="text-center mb-5">React File Uploads</h3>
+          <form>
+            <div className="mb-3">
+              <label className="form-label">
+                Caption:
+                <input
+                  required
+                  autoFocus
+                  type="text"
+                  id="caption"
+                  name="caption"
+                  className="form-control bg-light" />
+              </label>
+            </div>
+            <div className="d-flex justify-content-between align-items-center">
+              <input
+                required
+                type="file"
+                name="image"
+                accept=".mp3" />
+              <button type="submit" className="btn btn-primary">
+                Upload
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+};
