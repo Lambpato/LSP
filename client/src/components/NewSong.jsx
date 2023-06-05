318dce8d53c5f7f273fb559a6eec8f1fffb31f4f
Lamberto Martinez <lambpato@gmail.com>
2023-06-05 08:50:39 +0000

    created on submit function for form

--- client/src/components/NewSong.jsx
+++ client/src/components/NewSong.jsx
@@ -1,11 +1,33 @@
 export default function NewSong () {
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    try {
+      const response = await fetch ('/api/songs/uploads', {
+        method: "POST",
+        body: formData,
+      });
+
+      if(!response.ok) throw new Error(`Error Code: ${response.status} Error Message: It Boke`);
+
+      const postJson = await response.json();
+      console.log(postJson);
+    } catch (err) {
+      console.error('Error:', err)
+    }
+  };
+
   return (
     <div className="container">
       <div className="row min-vh-100 pb-5 justify-content-center align-items-center">
         <div className="col col-md-8">
           <h3 className="text-center mb-5">React File Uploads</h3>
-          <form>
+          <form onSubmit={handleSubmit
+
+
+
+          }>
             <div className="mb-3">
               <label className="form-label">
                 Caption:
