ab110ece6b846be50ef1e900a18c260d017b8cd5
Lamberto Martinez <lambpato@gmail.com>
2023-06-08 19:58:19 +0000

    fixed styling for the song page

--- client/src/Pages/SongPage.jsx
+++ client/src/Pages/SongPage.jsx
@@ -3,7 +3,7 @@ import Songs from "../components/Songs";
 
 export default function SongPage () {
   return  (
-    <div className="position=absolute top-0 start-0 bottom-0 end-0">
+    <div className="position-absolute top-0 start-0 bottom-0 end-0">
       <NavBar action={'Back'} />
       <Songs />
     </div>
