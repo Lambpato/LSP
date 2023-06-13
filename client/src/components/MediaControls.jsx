ebb2515064e9f687855d0bae2b1eaba13990dbfb
Lamberto Martinez <lambpato@gmail.com>
2023-06-13 05:37:31 +0000

    added functionality to the pause/play button

--- client/src/components/MediaControls.jsx
+++ client/src/components/MediaControls.jsx
@@ -1,27 +1,45 @@
-import { useState } from "react";
-import { SkipBackwardFill, PlayCircleFill, SkipForwardFill } from "react-bootstrap-icons";
+import { useState, useRef } from "react";
+import { SkipBackwardFill, PlayCircleFill, PauseCircleFill, SkipForwardFill } from "react-bootstrap-icons";
 import Slider from "./Slider";
 
-export default function MediaControls({song, onClickSkpBck, onClickPse, onClickSkp }) {
+export default function MediaControls({song, onClickSkpBck, onClickSkp }) {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [currentSong, setCurrentSong] = useState('');
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
   const [percentage, setPercentage] = useState(0);
 
-  const onClickPsePly = () => {
-    setIsPlaying(!isPlaying);
-  };
+  const audioRef = useRef();
 
-  const onChange = (e) => {
-    setPercentage(e.target.value);
-  };
+  const onChange = (e) => { setPercentage(e.target.value) };
+  const onPlay = () => {
+    const audio = audioRef.current;
+    if(!isPlaying) {
+       setIsPlaying(true);
+      audio.play();
+    } else if (isPlaying) {
+       setIsPlaying(false);
+      audio.pause();
+    };
+   };
 
   return (
   <div className="container">
   <h1>{song}</h1>
   <Slider onChange={onChange} percentage={percentage}/>
+  <audio
+  ref={audioRef}
+  src={song}
+  onLoadedData={(e) => {
+    console.log(e.currentTarget.duration);
+    setDuration(e.currentTarget.duration.toFixed(2));
+  }}
+  onTimeUpdate={(e) => {
+    console.log(e.currentTarget.duration);
+  }}
+  ></audio>
     <div>
       <button className="btn btn-link" onClick={onClickSkpBck}><SkipBackwardFill /></button>
-      <button className="btn btn-link" onClick={onClickPsePly}><PlayCircleFill /></button>
+      <button className="btn btn-link" onClick={onPlay}>{isPlaying ? <PlayCircleFill /> : <PauseCircleFill/>}</button>
       <button className="btn btn-link" onClick={onClickSkp}><SkipForwardFill /></button>
     </div>
   </div>
