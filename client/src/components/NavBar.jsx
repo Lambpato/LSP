6969e84fd07643fbd09df6339a406b1b6b90c7ed
Lamberto Martinez <lambpato@gmail.com>
2023-06-19 06:12:10 +0000

    fixed issue, onClick prop was missing from NavBar component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,11 @@
 import BackButton from "./BackButton"
 import Time from "./Time"
 
-export default function NavBar ({action}) {
+export default function NavBar ({action, onClick}) {
 
   return (
     <div className="d-flex flex-row justify-content-between bg-light bg-gradient fw-semibold">
-      <BackButton action={action} />
+      <BackButton action={action} onClick={onClick} />
       <Time />
     </div>
   )
