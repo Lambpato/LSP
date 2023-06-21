c1b477114feb208b5d567de44ee2c73e3e5fd1a5
Lamberto Martinez <lambpato@gmail.com>
2023-06-21 02:16:50 +0000

    removed test get request

--- server/server.js
+++ server/server.js
@@ -28,10 +28,6 @@ app.use(express.static(reactStaticDir));
 app.use(express.static(uploadsStaticDir));
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello World!' });
-});
-
 // register
 app.post('/api/users/register', async (req, res, next) => {
   try {
