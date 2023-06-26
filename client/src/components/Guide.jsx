9ebb67f857f251a6cc04afaaa4d78023f11f600c
Lamberto Martinez <lambpato@gmail.com>
2023-06-26 08:33:53 +0000

    completed accordion

--- client/src/components/Guide.jsx
+++ client/src/components/Guide.jsx
@@ -1,37 +1,108 @@
 export default function Guide ({ howTo }) {
 
   return (
-    <div class="accordion accordion-flush" id="accordionFlushExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+    <div className="accordion accordion-flush" id="howToAccordion">
+      <div className="accordion-item">
+        <h2 className="accordion-header">
+          <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+          Taking a Selfie
+          </button>
+        </h2>
+        <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#howToAccordion">
+          <div className="accordion-body">
+            <ul>
+              <li>Navigate to camera from home page</li>
+              <li>Click white shutter button</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion-item">
+        <h2 className="accordion-header">
+          <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+          Viewing Selfies Taken
+          </button>
+        </h2>
+        <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#howToAccordion">
+          <div className="accordion-body">
+            <ul>
+              <li>Navigate to saved photos from hompage</li>
+              <li>Click on the image url you would like to view</li>
+              <li>Click the same image to close or a new image</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion-item">
+        <h2 className="accordion-header">
+          <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+          Deleting a Selfie
+          </button>
+        </h2>
+        <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#howToAccordion">
+          <div className="accordion-body">
+            <ul>
+              <li>Select image you wish to delete</li>
+              <li>Press the button combination 'Shift' + 'D'</li>
+              <li>Hit the confirm button once sure</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion-item">
+        <h2 className="accordion-header">
+          <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseThree">
+          Saving a Song
+          </button>
+        </h2>
+        <div id="flush-collapseFour" className="accordion-collapse collapse" data-bs-parent="#howToAccordion">
+          <div className="accordion-body">
+            <ul>
+              <li>Navigate to new song from the homepage</li>
+              <li>Enter song name</li>
+              <li>Upload the mp3 file</li>
+              <li>Hit submit once everything has been filled</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion-item">
+        <h2 className="accordion-header">
+          <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseThree">
+          Listening to Saved Songs
+          </button>
+        </h2>
+        <div id="flush-collapseFive" className="accordion-collapse collapse" data-bs-parent="#howToAccordion">
+          <div className="accordion-body">
+            <ul>
+              <li>Navigate to saved songs from homepage</li>
+              <li>Click on anysong you would like to play</li>
+              <li>Pause and Play with media controls</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion-item">
+        <h2 className="accordion-header">
+          <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseThree">
+          Deleting Saved Songs
+          </button>
+        </h2>
+        <div id="flush-collapseSix" className="accordion-collapse collapse" data-bs-parent="#howToAccordion">
+          <div className="accordion-body">
+            <ul>
+              <li>Select song you wish to delete</li>
+              <li>Pres the button combination 'Shift' + 'D'</li>
+              <li>Hit the confirm button once sure</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
-    </div>
-  </div>
-</div>
   )
 };
