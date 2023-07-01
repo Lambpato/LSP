1d6c554e7ac011f26b481ecf6dc294dd56cc741b
Lamberto Martinez <lambpato@gmail.com>
2023-07-01 08:31:11 +0000

    ran into bug

--- client/src/components/MediaQuery.js
+++ client/src/components/MediaQuery.js
@@ -1,6 +1,6 @@
 import { useMediaQuery } from 'react-responsive'
 
 export default function MediaQuery(){
-  const desktop = useMediaQuery({ query: '(min-width: 750px)' });
+  const desktop = useMediaQuery({ query: '(min-width: 768px)' });
   return (desktop);
 }
