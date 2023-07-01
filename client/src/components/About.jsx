df8f4e015c178c3d20fc2cbffc32d2199779520f
Lamberto Martinez <lambpato@gmail.com>
2023-07-01 07:49:56 +0000

    added pointer to all buttons

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,13 +1,14 @@
 export default function About () {
+
   return (
-    <div className='col mt-5 '>
-      <h1 className='fs-1 fw-bold mb-5 text-center'>About</h1>
-      <p className='fs-4 text-start fw-semibold'>
+    <div className="col mt-5 ">
+      <h1 className="fs-1 fw-bold mb-5 text-center">About</h1>
+      <p className="fs-4 text-start fw-semibold">
         Hello! Welcome to LSP!
-        LSP is a media player which will allow you to store and play your favorite songs. You'll
+        LSP is a media player which will allow you to store and play your favorite songs. You"ll
         also be allowed to take selfies and very soon be able to play a library of games! All from you
         desktop to your mobile device!
       </p>
     </div>
   )
-}
+};
--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -13,17 +13,18 @@ export default function HomePage ({ icons }) {
         <CurrentIcon icons={icons} onClick={toggleCurrent} current={current} />
       </div>
     </div>
-  );
+  )
 };
 
  const CurrentIcon = ({ icons, onClick, current }) => {
     const dashboard = icons.map(icons =>
-      <li className='list-item-group' onClick={() => onClick(icons.id)}
+      <li role="button" className="list-item-group" onClick={() => onClick(icons.id)}
        key={icons.id}>
         <div><img style={{width: '100px'}} src={icons.imgUrl} alt={icons.name}/></div>
         {current === icons.id ? icons.children.map(child =>
-          <div className='d-flex flex-row align-items-center gap-2 align-items-center' key={child.childId} onClick={child.onClick}><img src={child.childUrl} alt={child.name} /><p  className='text-secondary'>{child.name}</p></div>)
+          <div className="d-flex flex-row align-items-center gap-2 align-items-center" key={child.childId} onClick={child.onClick}><img src={child.childUrl} alt={child.name} /><p  className="text-secondary">{child.name}</p></div>)
           : undefined}
       </li>);
-  return <ul className='list-unstyled list-group list-group-horizontal d-flex align-items-center gap-5'>{dashboard}</ul>
+
+  return <ul className="list-unstyled list-group list-group-horizontal d-flex align-items-center gap-5">{dashboard}</ul>
 };
