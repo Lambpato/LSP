699061f374f81fb4184f60dbe03d0986f04f5939
Lamberto Martinez <lambpato@gmail.com>
2023-07-07 08:41:39 +0000

    added error handling for registering for new account

--- client/src/components/RegIn.jsx
+++ client/src/components/RegIn.jsx
@@ -6,6 +6,18 @@ import { useNavigate } from 'react-router-dom';
 export default function RegisterPage ({ action, onLogIn }) {
   const navigate = useNavigate();
   const [error, setError] = useState();
+  const [password, setPassword] = useState('');
+  const numReg = /[0-9]+/;
+  const upReg = /[A-Z]+/;
+  const charReg = /[!@#$%^&*()]+/;
+
+  const passwordReq =
+    (password.length === 0) ? "A password is required."
+  : (password.length < 8) ? "Your password is too short."
+  : (password.length > 7 && !numReg.test(password)) ? "Please include a number"
+  : (password.length > 7 && !upReg.test(password)) ?  "Please include a capital letter"
+  : (password.length > 7 && !charReg.test(password)) ?  "Please include one of the following characters !, @, #, $, %, ^, &, *, (, or )"
+  : "Strong Password";
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -17,6 +29,7 @@ export default function RegisterPage ({ action, onLogIn }) {
       else if(result.user && result.token) onLogIn(result);
     } catch (err) {
       setError(err);
+      console.error(err);
     };
   };
 
@@ -30,7 +43,7 @@ export default function RegisterPage ({ action, onLogIn }) {
           required
           type="text"
           name="username"
-          placeholder="👤 Username"
+          placeholder="Username"
           className="rounded-5 border-1 text-center py-2"
           ></input>
         </label>
@@ -40,21 +53,19 @@ export default function RegisterPage ({ action, onLogIn }) {
           required
           type="password"
           name="password"
-          placeholder="🔑 Password"
+          placeholder="Password"
           className="rounded-5 border-1 text-center py-2"
+          onChange={e => setPassword(e.target.value)}
           ></input>
         </label>
-
       <button
         type="submit"
         className="rounded-pill mb-4 btn fw-semibold border border-dark"
         >
         {action}
       </button>
-
-      {error && <div style={{ color: 'red' }}>Error: {error.message}</div>}
+      {(error || (passwordReq && action === "register")) && <div style={{ color: 'red' }}>{error ? `Error: ${error.message}` : passwordReq}</div>}
     </form>
     </div>
-
   )
 };
