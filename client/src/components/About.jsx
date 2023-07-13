242ee5835e1664e5ee59cf871b87d5452dad6cdb
Lamberto Martinez <lambpato@gmail.com>
2023-07-13 08:21:45 +0000

    added new message to the wecomepage as well as new log in for examples

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,11 +4,14 @@ export default function About () {
     <div className="col mt-5 ">
       <h1 className="fs-1 fw-bold mb-5 text-center">About</h1>
       <p className="fs-4 text-start fw-semibold">
-        Hello! Welcome to LSP!
-        LSP is a media player which will allow you to store and play your favorite songs. You"ll
-        also be allowed to take selfies and very soon be able to play a library of games! All from you
-        desktop to your mobile device!
+        Hello! Welcome to Lamb Station Portable!
+        LSP is a media player which will allow you to store and play your favorite songs.
+        As well take selfies and store them to later view.
+        All from one application, without having to use multiple at once!
+        Want to try it out for yourself!? Use the following log in to test LSP out!
+
       </p>
+      <p className="fs-5 text-start fw-semibold">username: Example &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; password: Example1!</p>
     </div>
   )
 };
