e5eaba102fbaed0ae8579b194490b7191138117f
Lamberto Martinez <lambpato@gmail.com>
2023-07-25 04:56:21 +0000

    removed the data.sql

--- database/import-prod.sh
+++ database/import-prod.sh
@@ -13,8 +13,7 @@ fi
 
 if [ -n "$DATABASE_URL" ]; then
 psql "$DATABASE_URL" \
-    -f "$wd"/schema.sql \
-    -f "$wd"/data.sql
+    -f "$wd"/schema.sql
 else
 echo 'no DATABASE_URL environment variable set' 1>&2
 exit 1
