c9e85b8313bc10393416a8278855c1f1114dc74a
Lamberto Martinez <lambpato@gmail.com>
2023-07-25 07:13:08 +0000

    added data.sql

--- database/import-prod.sh
+++ database/import-prod.sh
@@ -13,7 +13,8 @@ fi
 
 if [ -n "$DATABASE_URL" ]; then
 psql "$DATABASE_URL" \
-    -f "$wd"/schema.sql
+    -f "$wd"/schema.sql \
+    -f "$wd"/data.sql
 else
 echo 'no DATABASE_URL environment variable set' 1>&2
 exit 1
