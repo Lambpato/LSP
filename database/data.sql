49d8e717ff0b8aea4a520edc062c4cdf5f57750d
Lamberto Martinez <lambpato@gmail.com>
2023-11-16 07:39:21 +0000

    fixed issue where new songs/selfie wasnt saving

--- database/data.sql
+++ database/data.sql
@@ -4,13 +4,13 @@ INSERT INTO "users"
   (1, 'example', '$argon2id$v=19$m=4096,t=3,p=1$4mtVqZVLhMaSPFneM1qnuA$Pf+rvBmaschmwxjWAt904zonM1DE8oU3XmfJTeAjocM', '2023-07-13T07:54:33.697Z');
 
 INSERT INTO "images"
-  ("imageId", "userId", "url", "createdAt")
+  ("userId", "url", "createdAt")
   VALUES
-  (1, 1, '/images/image-1689264867032.png', '2023-07-13T16:14:27.057Z');
+  (1, '/images/image-1689264867032.png', '2023-07-13T16:14:27.057Z');
 
 INSERT INTO "songs"
-  ("songId", "userId", "url", "song", "artist", "createdAt")
+  ("userId", "url", "song", "artist", "createdAt")
   VALUES
-  (1, 1, '/audio/1689235152806-audio.mp3', 'Crush Resist', 'Ecco2k', '2023-07-13T07:59:12.84Z'),
-  (2, 1, '/audio/1689235339915-audio.mp3', 'Girls Just Want To Have Fun (TS Remix)', 'Bladee & Ecco2k', '2023-07-13T08:02:20.003Z'),
-  (3, 1, '/audio/1689235476562-audio.mp3', 'Tokyo Nostalgia', 'XXXTentation', '2023-07-13T08:04:36.641Z');
+  (1, '/audio/1689235152806-audio.mp3', 'Crush Resist', 'Ecco2k', '2023-07-13T07:59:12.84Z'),
+  (1, '/audio/1689235339915-audio.mp3', 'Girls Just Want To Have Fun (TS Remix)', 'Bladee & Ecco2k', '2023-07-13T08:02:20.003Z'),
+  (1, '/audio/1689235476562-audio.mp3', 'Tokyo Nostalgia', 'XXXTentation', '2023-07-13T08:04:36.641Z');
